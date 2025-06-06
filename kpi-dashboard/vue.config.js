afa8e78991a8923359903460406e16969bebc3c5
Egor Ilyushin <egorilyu212@gmail.com>
2025-06-06 21:24:32 +0300

    final 4

--- kpi-dashboard/vue.config.js
+++ kpi-dashboard/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/KPDash_for_Small_Business/' // <- Убедитесь, что это название вашего репозитория!
+    : '/'
 })
