b7e1db5b91040658875e0a394c8dc98a7b99b587
Egor Ilyushin <egorilyu212@gmail.com>
2025-06-06 21:41:42 +0300

    index.js

--- kpi-dashboard/src/router/index.js
+++ kpi-dashboard/src/router/index.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import DashboardOverview from '../components/DashboardOverview.vue'; 
+import DashboardOverview from '../components/DashboardOverview.vue';
 
 const routes = [
   {
-    path: '/', 
-    name: 'DashboardOverview', 
-    component: DashboardOverview, 
+    path: '/',
+    name: 'DashboardOverview',
+    component: DashboardOverview,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/KPDash_for_Small_Business/' : '/'), // <-- ИЗМЕНИТЕ ЭТУ СТРОКУ!
   routes,
 });
 
--- kpi-dashboard/vue.config.js
+++ kpi-dashboard/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/KPDash_for_Small_Business/' // <- Убедитесь, что это название вашего репозитория!
+    ? '/KPDash_for_Small_Business/' 
     : '/'
 })
